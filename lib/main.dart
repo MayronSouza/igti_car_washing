3beb5c4cd7a449c96e67323cba5f9e6973d39790
MayronSouza <mayron.souza@gmz.solutions>
2021-05-01 10:37:53 -0300

    creating the initalRoute and the others routes - customers_register, book_service, book and customers_list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:igti_car_washing_ms/screens/Book.dart';
+import 'package:igti_car_washing_ms/screens/BookService.dart';
+import 'package:igti_car_washing_ms/screens/CustomersList.dart';
+import 'package:igti_car_washing_ms/screens/CustomersRegister.dart';
+import 'package:igti_car_washing_ms/screens/Home.dart';
 import 'package:igti_car_washing_ms/screens/MySplashScreen.dart';
 
 void main() {
@@ -7,5 +12,13 @@ void main() {
     home: Material(
       child: MySplashScreen(),
     ),
+    initialRoute: '/',
+    routes: {
+      '/home': (context) => Home(),
+      '/customers_register': (context) => CustomersRegister(),
+      '/book_service': (context) => BookService(),
+      '/book': (context) => Book(),
+      '/customers_list': (context) => CustomersList(),
+    },
   ));
 }
